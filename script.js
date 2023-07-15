0015ee028ffab9afc2056b0211878e25c9b1fbbf
Vishal <vishalpandey1718@gmail.com>
2023-07-15 11:06:02 +0530

    Final Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+/** @format */
+
+let arr = [
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin" },
+];
+
+function PrintDeveloperbyMap() {
+  //Write your code here , just console.log
+}
+
+function PrintDeveloperbyForEach() {
+  //Write your code here , just console.log
+}
+
+function addData() {
+  //Write your code here, just console.log
+}
+
+function removeAdmin() {
+  //Write your code here, just console.log
+}
+
+function concatenateArray() {
+  //Write your code here, just console.log
+}
